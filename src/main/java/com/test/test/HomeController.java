e96dacfe479799aab89e41bc20c1e80dda7ad4db
gautam1989 <gautam.verma.1989@gmail.com>
2016-05-11 20:26:06 +0800

    Update HomeController.java

--- src/main/java/com/test/test/HomeController.java
+++ src/main/java/com/test/test/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		System.out.println("Addedas");
+		System.out.println("removed");
 		return "home";
 	}
 	
